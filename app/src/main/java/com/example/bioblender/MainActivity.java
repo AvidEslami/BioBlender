c058eed58d2f2ef71fd82fd475da331500c9bc28
ArtinMolaeiForouhar <artinmolaeiforouhar@gmail.com>
2023-01-21 12:56:39 -0500

    basic python task

---
+++ app/src/main/java/com/example/bioblender/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.bioblender;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.chaquo.python.PyObject;
+import com.chaquo.python.Python;
+import com.chaquo.python.android.AndroidPlatform;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initPython();
+        TextView text1 = findViewById(R.id.textView1);
+        text1.setText(example());
+
+    }
+
+    private void initPython() {
+        Python.start(new AndroidPlatform(this));
+    }
+
+    private String example() {
+        Python python = Python.getInstance();
+        PyObject pythonFile = python.getModule("Sample");
+        return pythonFile.callAttr("test", "this is working").toString();
+    }
+}
+
