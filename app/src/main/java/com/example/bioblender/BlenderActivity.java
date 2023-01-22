99ccfe9157b8b31b23eff9cfd4782902b3ad6d93
Alex <alexandermotormail@gmail.com>
2023-01-22 00:07:43 -0500

    Attempting to use the python results in newAnimal

--- app/src/main/java/com/example/bioblender/BlenderActivity.java
+++ app/src/main/java/com/example/bioblender/BlenderActivity.java
@@ -2,14 +2,11 @@ package com.example.bioblender;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.chaquo.python.PyObject;
@@ -30,7 +27,7 @@ public class BlenderActivity extends AppCompatActivity implements View.OnClickLi
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.btn1:
+            case R.id.blendbtn:
                 String animal1 = an1.getText().toString();
                 String animal2 = an2.getText().toString();
 
@@ -42,12 +39,12 @@ public class BlenderActivity extends AppCompatActivity implements View.OnClickLi
                 PyObject pythonFile2 = python.getModule("imageAnimalScrape");
                 name = pythonFile2.callAttr("serpapi_get_google_images", name).toString();
 
-                Intent intent1 = new Intent(this, NewAnimal.class);
-                startActivity(intent1);
-                break;
-            case R.id.backbtn:
-                Intent intent2 = new Intent(this, MainActivity.class);
-                startActivity(intent2);
+                Intent intent = new Intent(this, NewAnimal.class);
+                intent.putExtra("name", name);
+                intent.putExtra("description", description);
+                intent.putExtra("ULR", URL);
+                intent.putExtra("reason", reason);
+                startActivity(intent);
                 break;
             default:
                 Toast.makeText(this, "You Died", Toast.LENGTH_SHORT).show();
@@ -61,11 +58,7 @@ public class BlenderActivity extends AppCompatActivity implements View.OnClickLi
         setContentView(R.layout.activity_blender);
         an1 = findViewById(R.id.animal1); // First animal view
         an2 = findViewById(R.id.animal2); // Second animal view
-        Button mixButton = findViewById(R.id.btn1);
+        Button mixButton = findViewById(R.id.blendbtn);
         mixButton.setOnClickListener(this);
-        Button backButton = findViewById(R.id.backbtn);
-        backButton.setOnClickListener(this);
-
-
     }
 }
--- app/src/main/java/com/example/bioblender/NewAnimal.java
+++ app/src/main/java/com/example/bioblender/NewAnimal.java
@@ -2,17 +2,29 @@ package com.example.bioblender;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class NewAnimal extends AppCompatActivity {
     ImageView picture;
+    private String name;
+    private String description;
+    private String URL;
+    private String reason;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_animal);
-        String URL = "https://media.licdn.com/dms/image/C4D03AQEEfIpJp8JjQg/profile-displayphoto-shrink_800_800/0/1644271955756?e=2147483647&v=beta&t=R8Utuk9j_0yyI1ek2u1xJiw52uIjHdkAFWxGhUBH3LM";
+        Intent i = getIntent();
+        name = i.getStringExtra("name");
+        description = i.getStringExtra("description");
+        URL = i.getStringExtra("URL");
+        reason = i.getStringExtra("reason");
         picture = findViewById(R.id.imageView1);
         new ImageLoadTask(URL, picture).execute();
+        TextView animal = findViewById(R.id.animal_name);
+        animal.setText(name);
     }
 }
